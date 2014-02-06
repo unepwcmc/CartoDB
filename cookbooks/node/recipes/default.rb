0d3637e5ba29a127c7190f64729e3ece3402590e
strtwtsn <stuart.watson@unep-wcmc.org>
2014-02-06 12:12:17 +0000

    Update default.rb

--- cookbooks/node/recipes/default.rb
+++ cookbooks/node/recipes/default.rb
@@ -8,9 +8,9 @@ package "npm"
 bash "configure node" do
 user "ubuntu"
 code <<-EOH
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-source /home/ubuntu/.bash_profile
-nvm install v0.8.9
-nvm use 0.8.9
+sudo -u ubuntu curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+sudo -u ubuntu source /home/ubuntu/.bash_profile
+sudo -u ubuntu nvm install v0.8.9
+sudo -u ubuntu nvm use 0.8.9
 EOH
 end
