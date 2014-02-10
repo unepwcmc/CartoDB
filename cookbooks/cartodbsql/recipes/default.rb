24f19b5e8d1571e1fde21bfb1e9a3392655e0abf
strtwtsn <stuart.watson@unep-wcmc.org>
2014-02-10 19:08:14 +0000

    Update default.rb

--- cookbooks/cartodbsql/recipes/default.rb
+++ cookbooks/cartodbsql/recipes/default.rb
@@ -1,5 +1,6 @@
 bash "Do CartoDBSQL stuff" do
 user "ubuntu"
+group "ubuntu"
 code <<-EOH
 git clone git://github.com/CartoDB/CartoDB-SQL-API.git /home/ubuntu/CartoDB-SQL-API
 cd /home/ubuntu/CartoDB-SQL-API
