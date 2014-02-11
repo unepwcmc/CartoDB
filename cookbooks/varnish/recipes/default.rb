633a790dc2f221a1f308784384c91e7396b08876
strtwtsn <stuart.watson@unep-wcmc.org>
2014-02-11 14:00:42 +0000

    Update default.rb

--- cookbooks/varnish/recipes/default.rb
+++ cookbooks/varnish/recipes/default.rb
@@ -2,3 +2,15 @@ package "varnish" do
   version "2.1.0-2cdb~precise1"
   action :install
 end
+
+service "varnish" do
+  service_name "postgresql"
+  supports :restart => true, :status => true, :reload => true
+end
+
+
+
+template "/etc/default/varnish" do
+source "varnish.erb"
+notifies :restart, resources(:service => "postgresql"), :immediately
+end
