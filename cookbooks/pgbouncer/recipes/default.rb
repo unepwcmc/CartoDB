9d37c47875c046e92c76299f59563bc3dcc9c29b
strtwtsn <stuart.watson@unep-wcmc.org>
2014-02-11 14:25:47 +0000

    Update default.rb

--- cookbooks/pgbouncer/recipes/default.rb
+++ cookbooks/pgbouncer/recipes/default.rb
@@ -1,5 +1,6 @@
 package "pgbouncer"
 
+
 template "/etc/pgbouncer/pgbouncer.ini" do
 owner "postgres"
 group "postgres"
@@ -12,6 +13,14 @@ owner "postgres"
 group "postgres"
 end
 
+template "/etc/default/pgbouncer" do
+source "default.erb"
+end
+
+template "/etc/init.d/pgbouncer" do
+source "init.erb"
+end
+
 bash "Setup Folders" do
 user "root"
 code <<-EOH
@@ -21,4 +30,11 @@ mkdir -p /var/log/pgbouncer
 touch /var/log/pgbouncer/pgbouncer.log
 chown -R postgres:postgres /var/log/pgbouncer
 EOH
+end
+
+
+service "pgbouncer" do
+  service_name "pgbouncer"
+  supports :restart => true, :status => true, :reload => true
+  action [ :enable, :restart ]
 end
